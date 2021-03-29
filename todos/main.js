12c62b84619aa594d1e91ae74ee1618acbbcc011
Roman <r.ledin.exceedteam@gmail.com>
2021-03-29 15:16:50 +0300

    final

--- todos/main.js
+++ todos/main.js
@@ -1,16 +1,16 @@
 /* eslint-disable no-undef */
 $(() => {
-  let arr = [];
+  let tasks = [];
   let id = 0;
-  const btn = $('#mainMarker');
-  const getter = document.getElementById('task');
-  const anotherGetter = document.getElementById('body');
+  const btn = $('#mainMarker'); // button
+  const getter = document.getElementById('task'); // clicker (get an id)
+  const getterWindow = document.getElementById('body');
 
   // LEFT COUNTER
   function itemsLeft() {
     let count = 0;
-    if (arr.length > 0) {
-      arr.forEach((obj) => {
+    if (tasks.length > 0) {
+      tasks.forEach((obj) => {
         if (!obj.isChecked) { count += 1; }
         $('#outputField').text(
           `${count} items left`,
@@ -21,27 +21,26 @@ $(() => {
         '0 items left',
       );
     }
-    console.log(arr);
   }
   itemsLeft();
 
-  // ENABLE-DISABLE MARKER
+  // HIDE BOTTOMBAR
   function btnCheck() {
-    if (arr.length === 0) {
+    if (tasks.length === 0) {
       $('.main__bottom').addClass('hidden'); $('.main__inputMark').addClass('hidden');
     } else { $('.main__bottom').removeClass('hidden'); $('.main__inputMark').removeClass('hidden'); }
   }
   btnCheck();
 
   // REBUILD ARR
-  function rebuild() {
-    arr.forEach((obj) => {
+  function rebuildTasks() {
+    tasks.forEach((obj) => {
       if (obj.isChecked) { $(`#list${obj.id}`).remove(); }
     });
-    arr = arr.filter((obj) => obj.isChecked === false);
+    tasks = tasks.filter((obj) => obj.isChecked === false);
   }
 
-  // INPUT
+  // INPUT TASKS
   $('#taskInput').keyup((event) => {
     const obj = {
       name: $('#taskInput').val(),
@@ -51,7 +50,7 @@ $(() => {
     };
 
     if ((event.keyCode === 13) && (obj.name !== '')) {
-      arr.push(obj);
+      tasks.push(obj);
 
       $('#task').append($(`
             <li class="task__listik" id="list${obj.id}">
@@ -68,27 +67,27 @@ $(() => {
     }
   });
 
-  // TOGGLE SOLO CHECKBOXES, CLICK ON DEL BUTT
+  // TOGGLE SOLO CHECKBOXES, DELETING SOLO TASKS
   getter.onclick = function toggleOnClick(e) {
     document.getElementById.innerHTML = e.target.id;
 
-    arr.forEach((obj, index) => {
+    tasks.forEach((obj, index) => {
       if (obj.id === +e.target.id) {
         if (!obj.isChecked) {
-          arr[index].isChecked = true;
+          tasks[index].isChecked = true;
           $(`#span${e.target.id}`).toggleClass('active').removeClass('inactive');
         } else {
-          arr[index].isChecked = false;
+          tasks[index].isChecked = false;
           $(`#span${e.target.id}`).toggleClass('inactive').removeClass('active');
         }
       }
 
       if (`button${obj.id}` === e.target.id) {
-        arr[index].status = 'deleted';
+        tasks[index].status = 'deleted';
 
-        const neededInd = arr.findIndex((object) => object.status === 'deleted');
+        const neededInd = tasks.findIndex((object) => object.status === 'deleted');
         $(`#list${obj.id}`).remove();
-        arr.splice(neededInd, 1);
+        tasks.splice(neededInd, 1);
       }
 
       if (`rewrite${obj.id}` !== e.target.id) {
@@ -96,62 +95,61 @@ $(() => {
         $(`#${obj.id}`).removeClass('hidden');
         $(`#span${obj.id}`).removeClass('hidden');
       }
-
     });
     itemsLeft();
     btnCheck();
   };
 
   // ONBLUR WHILE RENAME
-  anotherGetter.onclick = function toggleOnClickSecond(e) {
+  getterWindow.onclick = function renameOnBlur(e) {
     document.getElementById.innerHTML = e.target.id;
-    arr.forEach((obj, ) => {
-    if (`rewrite${obj.id}` !== e.target.id) {
-      $(`#rewrite${obj.id}`).addClass('hidden');
-      $(`#${obj.id}`).removeClass('hidden');
-      $(`#span${obj.id}`).removeClass('hidden');
+    tasks.forEach((obj) => {
+      if (`rewrite${obj.id}` !== e.target.id) {
+        $(`#rewrite${obj.id}`).addClass('hidden');
+        $(`#${obj.id}`).removeClass('hidden');
+        $(`#span${obj.id}`).removeClass('hidden');
       }
     });
-      itemsLeft();
-      btnCheck();
+    itemsLeft();
+    btnCheck();
   };
 
-  // ONE MORE CHECKER
-  checkitout = function checkItOutMark(e) {
+  // CHECK FUNCTION FOR MARK
+  checkitout = function checkItOutMark() {
     let count = 0;
-    debugger
-    arr.forEach(obj => {
-      if (obj.isChecked) { count += 1 }
+
+    tasks.forEach((obj) => {
+      if (obj.isChecked) { count += 1; }
     });
-    if (count === arr.length) {return e = true} else {return e = false}     
+    if (count === tasks.length) { return true; }
+    return false;
   };
 
-  // MARKER
+  // MARK ALL / UNMARK ALL
   btn.click(() => {
-    let e = checkitout();
-    debugger
+    const e = checkitout();
     if (e) {
-      arr.forEach((obj, index) => {
+      tasks.forEach((obj, index) => {
         $(`#span${obj.id}`).toggleClass('inactive').removeClass('active');
         $(`#${obj.id}`).prop('checked', false);
-        arr[index].isChecked = false;
+        tasks[index].isChecked = false;
       });
     } else {
-      arr.forEach((obj, index) => {
+      tasks.forEach((obj, index) => {
         if (!($(`#${obj.id}`).prop('checked'))) {
           $(`#span${obj.id}`).toggleClass('active').removeClass('inactive');
           $(`#${obj.id}`).prop('checked', true);
-          arr[index].isChecked = true;
-        };
+          tasks[index].isChecked = true;
+        }
       });
-    };
+    }
     btnCheck();
     itemsLeft();
   });
 
   // FILTERS
   $('.main__filter_active').click(() => {
-    arr.forEach((obj) => {
+    tasks.forEach((obj) => {
       if (obj.isChecked) {
         $(`#list${obj.id}`).addClass('hidden');
       } else {
@@ -160,7 +158,7 @@ $(() => {
     });
   });
   $('.main__filter_done').click(() => {
-    arr.forEach((obj) => {
+    tasks.forEach((obj) => {
       if (!obj.isChecked) {
         $(`#list${obj.id}`).addClass('hidden');
       } else {
@@ -169,21 +167,20 @@ $(() => {
     });
   });
   $('.main__filter_all').click(() => {
-    arr.forEach((obj) => {
+    tasks.forEach((obj) => {
       $(`#list${obj.id}`).removeClass('hidden');
     });
   });
 
-  // CLEAN COMPLETED
+  // CLEAN COMPLETED TASKS
   $('.main__cleaner').click(() => {
-    rebuild();
+    rebuildTasks();
     btnCheck();
   });
 
-  // RENAME
+  // RENAME ON DOUBLE CLICK
   getter.ondblclick = (function renaming(e) {
-    
-    arr.forEach((obj, index) => {
+    tasks.forEach((obj, index) => {
       if (`span${obj.id}` === e.target.id || `list${obj.id}` === e.target.id) {
         inputVal = obj.name;
         $(`#rewrite${obj.id}`).removeClass('hidden').val(inputVal);
@@ -192,7 +189,7 @@ $(() => {
 
         $(`#rewrite${obj.id}`).keyup((event) => {
           if ((event.keyCode === 13) && (obj.name !== '')) {
-            arr[index].name = $(`#rewrite${obj.id}`).val();
+            tasks[index].name = $(`#rewrite${obj.id}`).val();
 
             $(`#span${obj.id}`).text(obj.name);
 
@@ -203,18 +200,17 @@ $(() => {
             $(`#rewrite${obj.id}`).addClass('hidden');
             $(`#${obj.id}`).removeClass('hidden');
             $(`#span${obj.id}`).removeClass('hidden');
-            };
+          }
         });
-
       }
     });
   });
 
-  // BUT DELETE
+  // BUTTON DELETE HOVER
   getter.onmouseover = (function hoverOne(e) {
     document.getElementById.innerHTML = e.target.id;
 
-    arr.forEach((obj) => {
+    tasks.forEach((obj) => {
       if (`list${obj.id}` === e.target.id || `span${obj.id}` === e.target.id || obj.id === +e.target.id
             || `button${obj.id}` === e.target.id || `rewrite${obj.id}` === e.target.id) {
         $(`#button${obj.id}`).removeClass('hidden');
@@ -224,7 +220,7 @@ $(() => {
   getter.onmouseout = (function hoverTwo(e) {
     document.getElementById.innerHTML = e.target.id;
 
-    arr.forEach((obj) => {
+    tasks.forEach((obj) => {
       if (`list${obj.id}` === e.target.id || `span${obj.id}` === e.target.id || obj.id === +e.target.id
             || `button${obj.id}` === e.target.id || `rewrite${obj.id}` === e.target.id) {
         $(`#button${obj.id}`).addClass('hidden');
