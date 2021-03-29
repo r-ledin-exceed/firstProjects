7f0e2dff4c713f271d871d2dd5f536f7c1942982
Roman <r.ledin.exceedteam@gmail.com>
2021-03-29 11:30:48 +0300

    final cut

--- todos/main.js
+++ todos/main.js
@@ -1,42 +1,64 @@
-$(function() {
+/* eslint-disable no-param-reassign */
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+$(() => {
+  let arr = [];
+  let id = 0;
+  const btn = $('#mainMarker');
+  const getter = document.getElementById('task');
 
-    let arr = [],
-    id = 0,
-    btn = $("#mainMarker"),
-    getter = document.getElementById('task');
- 
-    
-    // LEFT COUNTER
-    function itemsLeft() {
-        let count = 0;
-        if (arr.length) {
-            arr.forEach(obj => {     
-                if (!obj.isChecked) { count++ };
-                $("#outputField").text(
-                    `${count} items left`
-                ) 
-            }); 
-        } else {
-            $("#outputField").text(
-                `0 items left`
-            ) 
-        };
+  // LEFT COUNTER
+  function itemsLeft() {
+    let count = 0;
+    if (arr.length) {
+      arr.forEach((obj) => {
+        if (!obj.isChecked) { count += count; }
+        $('#outputField').text(
+          `${count} items left`,
+        );
+      });
+    } else {
+      $('#outputField').text(
+        '0 items left',
+      );
+    }
+  }
+
+  // ENABLE-DISABLE MARKER
+  function btnCheck() {
+    let count = 0;
+    arr.forEach((obj) => {
+      if (obj.isChecked) { count += count; }
+    });
+    if (arr.length === count) { btn.prop('disabled', true); $('.markimg').addClass('transparent35'); } else { btn.prop('disabled', false); $('.markimg').removeClass('transparent35'); }
+    console.log(count);
+    if (arr.length === 0) {
+      $('.main__bottom').addClass('hidden'); $('.main__inputMark').addClass('hidden');
+    } else { $('.main__bottom').removeClass('hidden'); $('.main__inputMark').removeClass('hidden'); }
+  }
+  btnCheck();
+
+  // REBUILD ARR
+  function rebuild() {
+    arr.forEach((obj) => {
+      if (obj.isChecked) { $(`#list${obj.id}`).remove(); }
+    });
+    arr = arr.filter((obj) => obj.isChecked === false);
+  }
+
+  // INPUT
+  $('#taskInput').keyup((event) => {
+    const obj = {
+      name: $('#taskInput').val(),
+      id,
+      isChecked: false,
+
     };
 
-    // INPUT
-    $("#taskInput").keyup(function(event) {    
+    if ((event.keyCode === 13) && (obj.name !== '')) {
+      arr.push(obj);
 
-        let obj = {
-            name:$("#taskInput").val(),
-            id: id,
-            isChecked: false,
-     
-        };
-
-        if((event.keyCode == 13) && (obj.name != "")) {
-            arr.push(obj);
-
-            $("#task").append($(`
+      $('#task').append($(`
             <li class="task__listik" id="list${obj.id}">
             <input type = "text" id="rewrite${obj.id}" class="rewrite hidden">
             <input type="checkbox" id="${obj.id}"/>
@@ -44,237 +66,189 @@ $(function() {
             <button class="hidden" id="button${obj.id}">delete</button></li>
             `));
 
-            id++;
-            $("#taskInput").val("");
-            console.log(arr);
-            itemsLeft();
-            btnCheck();
-        };
-        
-    });
+      // eslint-disable-next-line no-plusplus
+      id++;
+      $('#taskInput').val('');
+      console.log(arr);
+      itemsLeft();
+      btnCheck();
+    }
+  });
 
-    // TOGGLE SOLO CHECKBOXES, CLICK ON DEL BUTT
-    getter.onclick = function(e) {
+  // TOGGLE SOLO CHECKBOXES, CLICK ON DEL BUTT
+  getter.onclick = function (e) {
+    // debugger
+    document.getElementById.innerHTML = e.target.id;
+    console.log(e.target.id);
+
+    arr.forEach((obj) => {
+      // debugger
+      if (obj.id === +e.target.id) {
+        if (!obj.isChecked) {
+          obj.isChecked = true;
+          $(`#span${e.target.id}`).toggleClass('active').removeClass('inactive');
+        } else {
+          obj.isChecked = false;
+          $(`#span${e.target.id}`).toggleClass('inactive').removeClass('active');
+        }
+      }
+
+      if (`button${obj.id}` === e.target.id) {
         // debugger
-        document.getElementById.innerHTML = e.target.id;
-        console.log(e.target.id);
- 
-        arr.forEach(obj => {
-            // debugger
-            if (obj.id === +e.target.id) { 
+        obj.status = 'deleted';
 
-                if (!obj.isChecked) {
-                    obj.isChecked = true;
-                    $("#span"+e.target.id).toggleClass("active").removeClass("inactive"); 
-                } else {
-                    obj.isChecked = false;
-                    $("#span"+e.target.id).toggleClass("inactive").removeClass("active"); 
-                }
-
-            };  
-
-            if ("button"+obj.id === e.target.id) {
-                // debugger
-                obj.status = "deleted";
-
-                let neededInd = arr.findIndex(obj => obj.status === "deleted");
-                    $("#list"+obj.id).remove();
-                    arr.splice(neededInd, 1)
-    
-            };  
-
-        });
-        itemsLeft();   
-        btnCheck();
-    };
-
-    //ENABLE-DISABLE MARKER
-    function btnCheck() {
-        let count = 0
-        arr.forEach(obj => {
-           if(obj.isChecked) {count++}
-       }); 
-       if (arr.length == count) {btn.prop("disabled", true); $(".markimg").addClass("transparent35")}
-        else {btn.prop("disabled", false); $(".markimg").removeClass("transparent35")};
-       console.log(count)
-       if (arr.length === 0) {$(".main__bottom").addClass("hidden"); $(".main__inputMark").addClass("hidden");
-        } else {$(".main__bottom").removeClass("hidden"); $(".main__inputMark").removeClass("hidden")}
-    };
+        const neededInd = arr.findIndex((object) => object.status === 'deleted');
+        $(`#list${obj.id}`).remove();
+        arr.splice(neededInd, 1);
+      }
+    });
+    itemsLeft();
     btnCheck();
-    
-    //MARKER
-    btn.click(function() {
+  };
 
-        arr.forEach(obj => {
+  // MARKER
+  btn.click(() => {
+    arr.forEach((obj) => {
+      if (!($(`#${obj.id}`).prop('checked'))) {
+        $(`#span${obj.id}`).toggleClass('active').removeClass('inactive');
+        $(`#${obj.id}`).prop('checked', true);
+        obj.isChecked = true;
+      }
 
-            if (!($("#"+obj.id).prop("checked"))) {
-                $("#span"+obj.id).toggleClass("active").removeClass("inactive");
-                $("#"+obj.id).prop("checked", true);
-                obj.isChecked = true;
-            }
+      // else if (($("#"+obj.id).prop("checked"))){
 
-            
-            // else if (($("#"+obj.id).prop("checked"))){
-                
-            // }
-            
-            // else {
-            //     $("#span"+obj.id).toggleClass("inactive").removeClass("active");
-            //     $("#"+obj.id).prop("checked", false);
-            //     obj.isChecked = false;
-            // } 
-            
-            // if($("input:checkbox").prop("checked")) {
-            //     $("#span"+obj.id).toggleClass("inactive").removeClass("active");
-            //     $("#"+obj.id).prop("checked", false);
-            //     obj.isChecked = false;
-            // } else {
-            //     $("#span"+obj.id).toggleClass("active").removeClass("inactive");
-            //     $("#"+obj.id).prop("checked", true);
-            //     obj.isChecked = true;
-            // }
+      // }
 
-        });
-        btnCheck();
-        itemsLeft();
+      // else {
+      //     $("#span"+obj.id).toggleClass("inactive").removeClass("active");
+      //     $("#"+obj.id).prop("checked", false);
+      //     obj.isChecked = false;
+      // }
+
+      // if($("input:checkbox").prop("checked")) {
+      //     $("#span"+obj.id).toggleClass("inactive").removeClass("active");
+      //     $("#"+obj.id).prop("checked", false);
+      //     obj.isChecked = false;
+      // } else {
+      //     $("#span"+obj.id).toggleClass("active").removeClass("inactive");
+      //     $("#"+obj.id).prop("checked", true);
+      //     obj.isChecked = true;
+      // }
     });
-    
-    //FILTERS
-    $(".main__filter_active").click(function() {
+    btnCheck();
+    itemsLeft();
+  });
 
-        arr.forEach(obj => {
-            if (obj.isChecked) { 
-                $("#list"+obj.id).addClass("hidden");
-            } else {
-                $("#list"+obj.id).removeClass("hidden");
-            }
-        });
-
+  // FILTERS
+  $('.main__filter_active').click(() => {
+    arr.forEach((obj) => {
+      if (obj.isChecked) {
+        $(`#list${obj.id}`).addClass('hidden');
+      } else {
+        $(`#list${obj.id}`).removeClass('hidden');
+      }
     });
-    $(".main__filter_done").click(function() {
-
-        arr.forEach(obj => {
-            if (!obj.isChecked) { 
-                // $("#list"+obj.id).css("display", "block");
-                $("#list"+obj.id).addClass("hidden");
-            } else {
-                $("#list"+obj.id).removeClass("hidden")
-            }
-                           
-        });
+  });
+  $('.main__filter_done').click(() => {
+    arr.forEach((obj) => {
+      if (!obj.isChecked) {
+        // $("#list"+obj.id).css("display", "block");
+        $(`#list${obj.id}`).addClass('hidden');
+      } else {
+        $(`#list${obj.id}`).removeClass('hidden');
+      }
     });
-    $(".main__filter_all").click(function() {
-
-        arr.forEach(obj => {
-            $("#list"+obj.id).removeClass("hidden");
-        });
+  });
+  $('.main__filter_all').click(() => {
+    arr.forEach((obj) => {
+      $(`#list${obj.id}`).removeClass('hidden');
     });
+  });
 
-    // CLEAN COMPLETED
-    $(".main__cleaner").click(function() {
-        //let needToDelete = arr.filter(obj => obj.isChecked === true)
-        for (let i=0; i< 10; i++ ){
-            arr.forEach(obj => {
-                if(obj.isChecked){
-                    obj.status = "deleted";
-            
+  // CLEAN COMPLETED
+  $('.main__cleaner').click(() => {
+    rebuild();
 
-                    let neededInd = arr.findIndex(obj => obj.status === "deleted");
-                    $("#list"+obj.id).remove();
-                    arr.splice(neededInd, 1)
+    // arr.forEach(obj => {
+    //     if(obj.isChecked){
+    //         debugger
+    //         obj.status = "deleted";
 
-                    // $("#list"+obj.id).remove();
-                    // arr.splice(0, 1)
-                };
-            
-            });
-        };
-        btnCheck();
-    });
+    //         let neededInd = arr.findIndex(obj => obj.status === "deleted");
+    //         $("#list"+obj.id).remove();
+    //         arr.splice(neededInd, 1)
 
-    //RENAME
-    getter.ondblclick = (function(e) {
-        document.getElementById.innerHTML = e.target.id;
-        
-        arr.forEach(obj => {
-            
-            if ("span"+obj.id === e.target.id || "list"+obj.id === e.target.id) { 
-                
-                inputVal = obj.name
-                $("#rewrite"+obj.id).removeClass("hidden").val(inputVal);
-                $("#"+obj.id).addClass("hidden");
-                $("#span"+obj.id).addClass("hidden");
+    //     };
+    // });
 
+    btnCheck();
+  });
 
-                $("#rewrite"+obj.id).keyup(function(event){
-                    if((event.keyCode == 13) && (obj.name != "") || (event.keyCode == 27)) {
-                        obj.name = $("#rewrite"+obj.id).val()
+  // RENAME
+  getter.ondblclick = (function (e) {
+    document.getElementById.innerHTML = e.target.id;
 
-                        $("#span"+obj.id).text(obj.name)
+    arr.forEach((obj) => {
+      if (`span${obj.id}` === e.target.id || `list${obj.id}` === e.target.id) {
+        inputVal = obj.name;
+        $(`#rewrite${obj.id}`).removeClass('hidden').val(inputVal);
+        $(`#${obj.id}`).addClass('hidden');
+        $(`#span${obj.id}`).addClass('hidden');
 
-                        console.log(arr);
+        $(`#rewrite${obj.id}`).keyup((event) => {
+          if (((event.keyCode === 13) && (obj.name !== '')) || (event.keyCode === 27)) {
+            obj.name = $(`#rewrite${obj.id}`).val();
 
-                        $("#rewrite"+obj.id).addClass("hidden");
-                        $("#"+obj.id).removeClass("hidden");
-                        $("#span"+obj.id).removeClass("hidden");
+            $(`#span${obj.id}`).text(obj.name);
 
-                        
-                        // getter.onclick(function(e) {
-                        //     document.getElementById.innerHTML = e.target.id;
-                        //     if (e.target.id != "rewrite"+obj.id) {
-                        //         $("#rewrite"+obj.id).addClass("hidden");
-                        //         $("#"+obj.id).removeClass("hidden");
-                        //         $("#span"+obj.id).removeClass("hidden");
-                        //     }
-                        // });
+            console.log(arr);
 
-                    };
+            $(`#rewrite${obj.id}`).addClass('hidden');
+            $(`#${obj.id}`).removeClass('hidden');
+            $(`#span${obj.id}`).removeClass('hidden');
 
-                });
-
-                // $("#rewrite"+obj.id).keyup(function(event){
-                //     if((event.keyCode == 27) && (obj.name != "")) {
-
-                //         $("#rewrite"+obj.id).addClass("hidden");
-                //         $("#"+obj.id).removeClass("hidden");
-                //         $("#span"+obj.id).removeClass("hidden");
-                //     };
-                // });
-
-            };
-
-        });
-    });
-
-    //BUT DELETE
-    getter.onmouseover = (function(e) {
-
-        document.getElementById.innerHTML = e.target.id;
-
-        arr.forEach(obj => {
-
-            if ("list"+obj.id === e.target.id || "span"+obj.id === e.target.id || obj.id === +e.target.id || 
-            "button"+obj.id === e.target.id || "rewrite"+obj.id === e.target.id) { 
-
-                $("#button"+obj.id).removeClass("hidden");
-
-            }; 
+            // getter.onclick(function(e) {
+            //     document.getElementById.innerHTML = e.target.id;
+            //     if (e.target.id != "rewrite"+obj.id) {
+            //         $("#rewrite"+obj.id).addClass("hidden");
+            //         $("#"+obj.id).removeClass("hidden");
+            //         $("#span"+obj.id).removeClass("hidden");
+            //     }
+            // });
+          }
         });
 
+        // $("#rewrite"+obj.id).keyup(function(event){
+        //     if((event.keyCode == 27) && (obj.name != "")) {
+
+        //         $("#rewrite"+obj.id).addClass("hidden");
+        //         $("#"+obj.id).removeClass("hidden");
+        //         $("#span"+obj.id).removeClass("hidden");
+        //     };
+        // });
+      }
     });
-    getter.onmouseout = (function(e) {
-        document.getElementById.innerHTML = e.target.id;
+  });
 
-        arr.forEach(obj => {
-            
-            if ("list"+obj.id === e.target.id || "span"+obj.id === e.target.id || obj.id === +e.target.id || 
-            "button"+obj.id === e.target.id || "rewrite"+obj.id === e.target.id) { 
+  // BUT DELETE
+  getter.onmouseover = (function (e) {
+    document.getElementById.innerHTML = e.target.id;
 
-                $("#button"+obj.id).addClass("hidden");
-               
-            }; 
-        });
-
+    arr.forEach((obj) => {
+      if (`list${obj.id}` === e.target.id || `span${obj.id}` === e.target.id || obj.id === +e.target.id
+            || `button${obj.id}` === e.target.id || `rewrite${obj.id}` === e.target.id) {
+        $(`#button${obj.id}`).removeClass('hidden');
+      }
     });
+  });
+  getter.onmouseout = (function (e) {
+    document.getElementById.innerHTML = e.target.id;
 
-}); 
+    arr.forEach((obj) => {
+      if (`list${obj.id}` === e.target.id || `span${obj.id}` === e.target.id || obj.id === +e.target.id
+            || `button${obj.id}` === e.target.id || `rewrite${obj.id}` === e.target.id) {
+        $(`#button${obj.id}`).addClass('hidden');
+      }
+    });
+  });
+});
